fbc43132bc6d3266620ebb08b64416cc85489d1f
Sinomtha Mzamo <mzamo.sinomthaayakhanya@gmail.com>
2024-10-20 00:26:05 +0200

    added realtime validation for all text fields of testimonial form

--- forms/submitTestimonial.js
+++ forms/submitTestimonial.js
@@ -1,3 +1,5 @@
+var isValid  = false;
+
 const form = document.querySelector('form');
 
 const modal = document.getElementById('exampleModal');
@@ -5,6 +7,18 @@ const formMultiStep = document.querySelector("[testimonial-multi-step]");
 const formSteps = [...document.querySelectorAll("[testimonial-step]")];
 
 let currentStep = 0; // Initialize current step
+
+const errorMessagesMap = {
+    testimonial: '',
+    tagline: '',
+    username: '',
+    name: '',
+    image: '',
+};
+
+const getFormInputElementById = (elementId) => {
+    return document.getElementById(elementId);
+}
 
 modal.addEventListener('show.bs.modal', () => {
     currentStep = 0; // Reset to first step
@@ -45,6 +59,55 @@ const validateUsername = async (username) => {
 
 };
 
+const validateName = (name) => {
+    // validate name field: should be filled in
+    if(!name.value.trim()) {
+        errorMessagesMap.name = 'Name is required.';
+        isValid = false;
+    } else {
+        errorMessagesMap.name = '';
+        isValid = true;
+    }
+
+}
+
+const validate = async (fieldName) => {
+    switch(fieldName){
+        case 'username':
+            const username_value = getFormInputElementById(fieldName).value;
+            console.log(username_value);
+            // validate username field: should be filled in and should be valid
+            if(!username_value.trim() ||!(await validateUsername(username_value))) {
+                errorMessagesMap.username = 'A valid WeThinkCode username is required.';
+                isValid = false;
+            } else {
+                errorMessagesMap.username = '';
+                isValid = true;
+            }
+            console.log(errorMessagesMap.username);
+            console.log(username_value+' is valid? : '+`${isValid}`)
+            console.log(isValid ? 'valid username!': 'invalid username!' );
+            displayErrorMessage(fieldName);            
+        case 'name':
+            validateName(getFormInputElementById(fieldName));
+            console.log(isValid ? 'valid name!': 'invalid name!' );
+            console.log(isValid)
+            displayErrorMessage(fieldName);
+        case 'tagline': 
+            // validate tagline character count
+            if (getFormInputElementById('tagline').value.length < 30) {
+                errorMessagesMap.tagline = 'Tagline must at least be 30 characters.';
+                isValid = false;
+            } else {
+                errorMessagesMap.tagline = '';
+                isValid = true;
+            }
+            console.log(isValid ? 'valid tagline!': 'invalid tagline!' );
+            console.log(isValid)
+            displayErrorMessage(fieldName);
+    }
+}
+
 const reportWordCount = (currentCharacterCountId, testimonialItemId, wordCountThreshold) => {
     const characterCount = document.getElementById(currentCharacterCountId);
     const testimonial = document.getElementById(testimonialItemId);
@@ -55,6 +118,15 @@ const reportWordCount = (currentCharacterCountId, testimonialItemId, wordCountTh
     } else {
         characterCount.style.color = 'inherit';
     }
+    // validate tagline character count
+    if (testimonial.value.length < 30) {
+        errorMessagesMap[testimonialItemId] = `${capitalize(testimonialItemId)} must at least be 30 characters.`;
+        isValid = false;
+    } else {
+        errorMessagesMap[testimonialItemId] = '';
+        isValid = true;
+    }
+    displayErrorMessage(testimonialItemId);
     console.log(testimonial.value.length);
 };
 
@@ -86,22 +158,24 @@ const handleSubmit = async (event) => {
 
 form.addEventListener('submit', handleSubmit);
 
+const getTestimonial = () => document.getElementById('testimonial');
+const getTagline = () => document.getElementById('tagline');
+const getUsername = () => document.getElementById('username');
+const getName = () => document.getElementById('name');
+const getImage = () => document.getElementById('image');
+
+
+
 const validateForm = async () => {
     // get form fields
-    const testimonial = document.getElementById('testimonial');
-    const tagline = document.getElementById('tagline');
-    const username = document.getElementById('username');
-    const name = document.getElementById('name');
-    const image = document.getElementById('image');
+    const testimonial = getFormInputElementById('testimonial');
+    const tagline = getFormInputElementById('tagline');
+    const username = getFormInputElementById('username');
+    const name = getFormInputElementById('name');
+    const image = getFormInputElementById('image');
 
     let isValid = true;
-    const errorMessagesMap = {
-        testimonial: '',
-        tagline: '',
-        username: '',
-        name: '',
-        image: '',
-    };
+    
 
     // validate testimonial character count
     if (testimonial.value.length < 30) {
@@ -145,4 +219,14 @@ const validateForm = async () => {
     return isValid;
 
 };
+
+const displayErrorMessage = (fieldName) => {
+    const errorElement = getFormInputElementById(`${fieldName}-error`);
+    const inputArea = getFormInputElementById(fieldName);
+    errorElement.textContent = errorMessagesMap[fieldName];
+    console.log('this is what you should see: '+errorElement.textContent);
+    errorElement.style.display = errorMessagesMap[fieldName] ? 'block' : 'none';
+    inputArea.classList.toggle('error-focus', !isValid);
+
+}
 
